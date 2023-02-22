7cb81b093d73e8c3aaa6c5a66af1b3ff8446f962
Real.Siao <119853210+realsiao@users.noreply.github.com>
2023-02-23 00:38:41 +0500

    Create c_request.c

---
+++ c_request.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char message[1000], server_reply[2000];
+
+    // Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr("216.58.194.174");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+
+    // Connect to remote server
+    if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+
+    puts("Connected\n");
+
+    // Send some data
+    message = "GET / HTTP/1.1\r\n\r\n";
+    if (send(sock, message, strlen(message), 0) < 0)
+    {
+        puts("Send failed");
+        return 1;
+    }
+
+    // Receive a reply from the server
+    if (recv(sock, server_reply, 2000, 0) < 0)
+    {
+        puts("recv failed");
+    }
+
+    puts("Reply received\n");
+    puts(server_reply);
+
+    close(sock);
+    return 0;
+}
